45b47f5a0f61762bf84921e8713602f9610767e5
ISHIKA SOLANKI <ishikasolanki682@gmail.com>
2025-05-13 13:54:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 function fibonacci(num) {
-    if (num < 0) return null; // handle invalid input
-    if (num === 0) return 0;
-    if (num === 1) return 1;
+    if (num === 1) return 0;
+    if (num === 2) return 1;
 
-    let prev = 0;
-    let curr = 1;
-
-    for (let i = 2; i <= num; i++) {
-        let next = prev + curr;
-        prev = curr;
-        curr = next;
+    let a = 0, b = 1;
+    for (let i = 3; i <= num; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
     }
-
-    return curr;
+    return b;
 }
 
 module.exports = fibonacci;
-fibonacci(1); // 0
-fibonacci(2); // 1
-fibonacci(3); // 
